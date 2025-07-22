264af31c477ba48d4fdd757cf8c8881dbd4d5a61
kridsanapakron <dgsx.64854@gmail.com>
2025-07-22 21:55:53 +0700

    v1.0

---
+++ paramikolab.py
@@ -0,0 +1,19 @@
+import paramiko
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+USERNAME = os.getenv("SSH_USERNAME")
+PRIVATE_KEY_PATH = os.getenv("PRIVATE_KEY_PATH")
+DEVICE_IP = os.getenv("DEVICE_IP")
+
+key = paramiko.RSAKey.from_private_key_file(PRIVATE_KEY_PATH)
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(hostname=DEVICE_IP, username=USERNAME, pkey=key)
+
+stdin, stdout, stderr = client.exec_command("sh ip int br")
+print(stdout.read().decode())
+client.close()
